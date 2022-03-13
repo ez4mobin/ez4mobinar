22bfa483d6d5352fd818a8425e3ba1164c223e32
ez4mobin <ez4mobinar@gmail.com>
2022-03-13 12:24:58 +0330

    exersises1.py

---
+++ exersises1.py/01.py
@@ -0,0 +1,7 @@
+a = int(input())
+ if a % 3 == 0 and a % 5 == 0:
+     print("foobar")
+elif a % 5 == 0:
+    print("foo")
+elif a % 3 == 0:
+    print("bar")
---
+++ exersises1.py/03.py
@@ -0,0 +1,6 @@
+a = int(input())
+max = int(input())
+if max < a:
+    print((a - max)//5 * 20000)
+else:
+    print("driver observe  the speed limite")
