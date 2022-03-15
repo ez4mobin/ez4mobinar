fea6de7f0cc9a665465936236aa67114cf52029a
ez4mobin <ez4mobinar@gmail.com>
2022-03-15 12:11:02 +0330

    commit messages

---
+++ exersises2.py/01.py
@@ -0,0 +1,14 @@
+name = "name"
+age ="age"
+nationalid = "nationalid"
+name1 = str(input())
+age1 = str(input())
+nationalid1 = str(input())
+name2 = str(input())
+age2 = str(input())
+nationalid2 = str(input())
+print(f"|{name:<15}|{age:<15}|{nationalid:<15}")
+print("-----------------------------------------------")
+print(f"|{name1:<15}|{age1:<15}|{nationalid1:<15}")
+print("-----------------------------------------------")
+print(f"|{name2:<15}|{age2:<15}|{nationalid2:<15}")
---
+++ exersises2.py/03.py
@@ -0,0 +1,20 @@
+name = "HeLLo WorLD"
+print(name[0])
+print(name[2])
+print(name[3])
+print(name[5])
+print(name[8])
+print(name[9])
+o = "o"
+r = "r"
+a = print(ord("H")) #72
+b = print(ord("L")) #76
+c = print(ord("W")) #87
+d = print(ord("D")) #68
+e = print(ord("h")) #104
+f = print(ord("l")) #108
+g = print(ord("w")) #119
+h = print(ord("d")) #100
+print(chr(104),"e",chr(108),chr(108),"o",end="",sep="")
+print(end=" ",sep=" ")
+print(chr(119),"o","r",chr(108),chr(100),sep="",end=" ")
